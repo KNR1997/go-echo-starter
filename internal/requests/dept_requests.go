8e774d9f789e32b3187fe04bbeb7dd711ec68f52
KNR1997 <kethakaranasinghe@gmail.com>
2026-06-06 23:22:53 +0530

    refactor: BasicDept validation

--- internal/requests/dept_requests.go
+++ internal/requests/dept_requests.go
@@ -4,15 +4,13 @@ import validation "github.com/go-ozzo/ozzo-validation/v4"
 
 type BasicDept struct {
 	Name  string `json:"name" validate:"required" example:"Staff"`
-	Desc  string `json:"desc" validate:"required" example:"Staff role"`
-	Order string `json:"order" validate:"required" example:"1"`
+	Desc  string `json:"desc" example:"Staff role"`
+	Order string `json:"order" example:"1"`
 }
 
 func (bp BasicDept) Validate() error {
 	return validation.ValidateStruct(&bp,
 		validation.Field(&bp.Name, validation.Required),
-		validation.Field(&bp.Desc, validation.Required),
-		validation.Field(&bp.Order, validation.Required),
 	)
 }
 
