379dd9770a79c884e3e5cf8e4e230c6e2a464b87
KNR1997 <kethakaranasinghe@gmail.com>
2026-06-19 20:37:15 +0530

    feat(model): AuditLog

---
+++ internal/models/audit_log.go
@@ -0,0 +1,33 @@
+package models
+
+import (
+	"encoding/json"
+	"time"
+)
+
+type AuditLog struct {
+	ID           uint            `gorm:"primaryKey" json:"id"`
+	UserID       int             `gorm:"index;not null" json:"user_id"`
+	Username     string          `gorm:"size:64;index;default:''" json:"username"`
+	Module       string          `gorm:"size:64;index;default:''" json:"module"`
+	Summary      string          `gorm:"size:128;index;default:''" json:"summary"`
+	Method       string          `gorm:"size:10;index;not null" json:"method"`
+	Path         string          `gorm:"size:255;index;default:''" json:"path"`
+	Status       int             `gorm:"index;default:-1" json:"status"`
+	ResponseTime int             `gorm:"index;default:0" json:"response_time"`
+	RequestArgs  json.RawMessage `gorm:"type:jsonb" json:"request_args"`
+	ResponseBody json.RawMessage `gorm:"type:jsonb" json:"response_body"`
+	CreatedAt    time.Time       `gorm:"index;autoCreateTime" json:"created_at"`
+	UpdatedAt    time.Time       `gorm:"autoUpdateTime" json:"updated_at"`
+}
+
+type AuditLogSimple struct {
+	ID       uint   `gorm:"primaryKey" json:"id"`
+	UserID   int    `gorm:"index;not null" json:"user_id"`
+	Username string `gorm:"size:64;index;default:''" json:"username"`
+	Module   string `gorm:"size:64;index;default:''" json:"module"`
+}
+
+func (AuditLog) TableName() string {
+	return "audit_logs"
+}
